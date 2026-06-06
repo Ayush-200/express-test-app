f4a89bb16aa9256e74aa9855bb1233ee31bbbeb6
Ayush Bhatia <ayushbhatia456@gmail.com>
2026-06-07 03:41:21 +0530

    dockerfile added

---
+++ test.js
@@ -0,0 +1,19 @@
+// for(let i=0; i<=5; i++){
+//     setTimeout(() => {
+//         console.log(i)}, 5000);
+// }
+
+// if(!false){
+//     var i  = 10;
+//     console.log(i);
+// }
+
+// console.log(i);
+
+function test() {
+    var name = "John";
+    console.log(name);
+}
+
+test();
+console.log(name);
